5201826dbd96886ee107589b9c88c7c411a436a0
Jesus Alberto Garcia <jes.alberto96@gmail.com>
2024-09-01 23:22:22 -0500

    Ajuste en variables de entorno

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,10 +4,10 @@ import { get } from 'env-var';
 
 export const envs = {
     PORT: get('PORT').required().asPortNumber(),
-    WEB_SERVICE_URL: get('WEB_SERVICE_URL').required().asString(),
     MONGO_URL: get('MONGO_URL').required().asString(),
     MONGO_DB_NAME: get('MONGO_DB_NAME').required().asString(),
     MONGO_DB_USERNAME: get('MONGO_DB_USERNAME').required().asString(),
     MONGO_DB_PASSWORD: get('MONGO_DB_PASSWORD').required().asString(),
-    JWT_SEED: get('JWT_SEED').required().asString(),
+    // WEB_SERVICE_URL: get('WEB_SERVICE_URL').required().asString(),
+    // JWT_SEED: get('JWT_SEED').required().asString(),
 }
